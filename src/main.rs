647678f98f5902bca92268840a7c8a41bbb9c261
Ichim8 <155743208+Ichim9@users.noreply.github.com>
2025-06-05 18:13:55 +0100

    car alert

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+// STREAM #3 :D
+extern crate clap;
+extern crate reqwest;
+use clap::Command;
+use reqwest::blocking::get;
+fn main() {
+    let cmd = shell().get_matches();
+    match cmd.subcommand(){
+        Some(("get",_))=>{
+            match get("https://cataas.com/cat"){
+                Ok(k)=>{
+                    match k.bytes(){
+                        Ok(k)=>{
+                            match std::fs::write("catpic.png",k){
+                                Ok(_)=>{}
+                                Err(e)=>{
+                                    println!("Error writing file");
+                                    println!("Error will be displayed below:\n\n{e}\n\n");
+                                }
+                            }
+                        }
+                        Err(e)=>{
+                            println!("Error converting to bytes");
+                            println!("Error will be displayed below:\n\n{e}\n\n");
+                        }
+                    }
+                }
+                Err(e)=>{
+                    println!("Error connecting to catAAS?\n is your wifi broken?");
+                    println!("Error will be displayed below:\n\n{e}\n\n");
+                }
+            }
+        }
+        _=>{}
+    }
+}
+
+fn shell() -> Command{
+    Command::new("catpics").about("I wonder what the name of this implies, I personally haven't a clue!").subcommand(
+        Command::new("get")
+    ).subcommand_required(true)
+}
